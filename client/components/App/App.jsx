babb5caaa9890b7a56c865c8d71b21bb6e903228
ntoung <nmtoung@gmail.com>
2017-02-16 12:13:15 -0800

    Initial commit for client components

---
+++ client/components/App/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import styles from './App.css';
+// import NavBar from '../NavBar/NavBar';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="">
+        
+      </div>  
+    );
+  }
+}
+
+App.propTypes = {};
+
+export default App;

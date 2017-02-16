ad31584aaf204c67402da26306e2b88aa2c84782
ntoung <nmtoung@gmail.com>
2017-02-16 12:39:57 -0800

    Add empty server config

---
+++ server/server.config.js
@@ -0,0 +1,21 @@
+
+const config = {
+  HOST: '', 
+  PORT: process.env.PORT || 8000,
+
+  env: process.env.NODE_ENV || '', // default
+
+  // Environments
+  dev: 'development',
+  testing: 'testing',
+  staging: 'staging',
+  prod: 'production',
+
+  // Database
+  database: {
+    local: '',
+    postgresql: ''
+  }
+}
+
+module.exports = config;

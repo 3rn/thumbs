ab1a57e704c1e6d70a3a3681e2047880ffaf70b2
ntoung <nmtoung@gmail.com>
2017-02-16 16:24:15 -0800

    Add file structure for redux

---
+++ client/reducers/reducer_2.js
@@ -0,0 +1,12 @@
+// Import actions
+import {  } from '../actions/index';
+
+
+export default function EnglishCurrentReducer(state=0, action) {
+  const current = state;
+  switch(action.type){
+    
+    default:
+      return state;
+  }
+}

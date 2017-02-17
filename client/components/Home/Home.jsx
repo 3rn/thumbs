eafc6d686eeea22b56b12031b6722114c344ca01
Ranit Dubey <ranitdubey@gmail.com>
2017-02-16 17:35:25 -0800

    client emitting vote

--- client/components/Home/Home.jsx
+++ client/components/Home/Home.jsx
@@ -5,11 +5,33 @@ let socket = io('http://localhost:8000');
 import styles from './Home.scss';
 
 class Home extends React.Component {
-
+  constructor(props) {
+    super(props);
+    socket.on('vote', (payload) => {   
+      console.log(payload);
+    });
+  }
+  
   render() {
+    
+    const handlePress = (e) => {
+      socket.emit('vote', { option: e.target.value});
+    };
+    
     return (
       <div className={styles.home}>
-        <h1>Home</h1>
+        <div>
+          <h1>Home</h1>
+        </div>
+        <div>
+          <button value="1" onClick={handlePress}> Thumbs 1 </button> <span> 0 </span>
+        </div>
+        <div>
+          <button value="2" onClick={handlePress}> Thumbs 2 </button> <span> 0 </span>
+        </div>
+        <div>
+          <button value="3" onClick={handlePress}> Thumbs 3 </button> <span> 0 </span>
+        </div>
       </div>
     );
   }

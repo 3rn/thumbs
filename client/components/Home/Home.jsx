111614aba6c7215ec37b136d315245da8e1fdaa0
ntoung <nmtoung@gmail.com>
2017-02-16 16:21:09 -0800

    Replace css with sass

--- client/components/Home/Home.jsx
+++ client/components/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-// import styles from './Home.css';
+import styles from './Home.scss';
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="">
+      <div className={styles.home}>
         <h1>Home</h1>
       </div>
     );

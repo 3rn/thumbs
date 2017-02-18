e1487e819d56f650472cefa84d7b7ff84ecc4c22
ryanmgray <gray.ryanm@gmail.com>
2017-02-18 11:50:09 -0800

    Remove middleware from store

--- client/main.jsx
+++ client/main.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store.js';
+
 import routes from './containers/Router/Routes';
 import Home from './containers/Home/Home';
-
-// Redux
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
-
-const store = createStore(reducers);
 
 render(
   <Provider store={store}>
--- client/store.js
+++ client/store.js
@@ -1,11 +1,4 @@
 import { createStore } from 'redux';
-
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise-middleware';
-
 import reducer from './reducers';
-
-const middleware = applyMiddleware(promise(), thunk, logger());
 
 export default createStore(reducer);

73fae126b4073264432a75f0a2b3876f753b8aea
rwhuber <rwhuber13@gmail.com>
2017-02-18 11:52:46 -0800

    Update App.jsx

--- client/containers/App/App.jsx
+++ client/containers/App/App.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 
 class App extends React.Component {
-
-  handleEventClick(event) {
-    let {dispatch} = this.props;
-    dispatch(app.handleEventClick(event));
-  }
-
   render() {
     return (
       <div className="">

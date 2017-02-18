549211d62172875ad0211fc5e6c0a4c1d1b71e43
ryanmgray <gray.ryanm@gmail.com>
2017-02-18 10:47:04 -0800

    Clean up app container, remove redux and socket.io

--- client/containers/App/App.jsx
+++ client/containers/App/App.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-// import styles from './App.css';
-// import NavBar from '../NavBar/NavBar';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+
+  handleEventClick(event) {
+    let {dispatch} = this.props;
+    dispatch(app.handleEventClick(event));
   }
 
   render() {
     return (
       <div className="">
         { this.props.children }
-      </div>  
+      </div>
     );
   }
 }
-
-App.propTypes = {};
 
 export default App;

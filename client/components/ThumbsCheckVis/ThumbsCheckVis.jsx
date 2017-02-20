cdb7ec802c37067481612f14b8b7fc1433362c10
Richard Huber <rwhuber13@gmail.com>
2017-02-20 12:10:34 -0800

    Initialize thumbs check visualization

---
+++ client/components/ThumbsCheckVis/ThumbsCheckVis.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { VictoryBar } from 'victory';
+
+class ThumbsCheckVis extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default ThumbsCheckVis;

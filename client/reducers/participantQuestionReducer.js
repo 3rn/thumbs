765d01070870472135bca532ff294a9438add0d3
ryanmgray <gray.ryanm@gmail.com>
2017-02-21 19:40:33 -0800

    Add participant question reducer

--- client/reducers/participantQuestionReducer.js
+++ client/reducers/participantQuestionReducer.js
@@ -1,9 +1,7 @@
-export default function voteReducer(state = [0, 0, 0], action) {
+export default function participantQuestion(state = 0, action) {
   switch (action.type) {
-    case 'UPDATE':
-      var newCount = state.slice(0);
-      var index = parseInt(action.value) - 1;
-      newCount[index] += 1;
+    case 'PARTICIPANT_QUESTION':
+      const newCount = state + 1;
       return newCount;
   }
   return state;

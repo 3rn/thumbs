89ea189a7944a8fa52bcbccd99187254bd85dfbd
ntoung <nmtoung@gmail.com>
2017-02-24 14:17:43 -0800

    Working validation of room codes

--- client/components/Home/Home.jsx
+++ client/components/Home/Home.jsx
@@ -6,11 +6,39 @@ import { Link } from 'react-router';
 class Home extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      validRoom: false
+    }
+
+    this.onEnterRoomChange = this.onEnterRoomChange.bind(this);
+    this.onEnterRoomSubmit = this.onEnterRoomSubmit.bind(this);
   }
           // <ul>
           //   <li><Link to="/participant">Participant View</Link></li>
           //   <li><Link to="/presenter">Presenter View</Link></li>
           // </ul>
+
+
+  enterRoom () {
+
+  }
+
+  onEnterRoomChange(e) {
+    let roomCode = e.target.value.toUpperCase();
+  
+    if (roomCode.length === 4 && roomCode === 'ASDF') {
+      this.setState({'validRoom': true});
+    } else {
+      this.setState({'validRoom': false});
+    }
+
+  }
+
+  onEnterRoomSubmit(e) {
+    
+    e.preventDefault();
+  }
 
   render() {
     return (
@@ -19,16 +47,49 @@ class Home extends React.Component {
 
           <div className={styles.container}>
             <div className={styles.link}>
-              <input 
-                type="text" 
-                placeholder="Enter Room Code: ABCD"
-                maxLength="4"
-                // pattern="([a-zA-Z])\w\S+"
-                required
-              ></input>
-              <button className={styles.enterRoom}>
-                  <i className="fa fa-sign-in" aria-hidden="true"></i>
-              </button>
+
+              <form id="enterRoom"
+                onSubmit={this.onEnterRoomSubmit}
+              >
+                <input 
+                  onChange={this.onEnterRoomChange}
+                  
+                  type="text" 
+                  placeholder="Enter Room Code: ABCD"
+                  maxLength="4"
+                  // pattern=".{4,}([a-zA-Z])\w\S+"
+                  // pattern=".{4,}"
+                  required
+                ></input>
+                <button className={styles.enterRoom}>
+                    <span className={(this.state.validRoom)?styles.validRoom:''} >
+                    <i className="fa fa-sign-in" aria-hidden="true"></i>
+                    </span>
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.link}>
+              <form id="createRoom"
+                onSubmit={this.onEnterRoomSubmit}
+              >
+                <input 
+                  onChange={this.onEnterRoomChange}
+                  
+                  type="text" 
+                  placeholder="Enter Room Code: ABCD"
+                  maxLength="4"
+                  // pattern=".{4,}([a-zA-Z])\w\S+"
+                  // pattern=".{4,}"
+                  required
+                ></input>
+                <button className={styles.enterRoom}>
+                    <span className={(this.state.validRoom)?styles.validRoom:''} >
+                    <i className="fa fa-sign-in" aria-hidden="true"></i>
+                    </span>
+                </button>
+              </form>
             </div>
           </div>
         </div>

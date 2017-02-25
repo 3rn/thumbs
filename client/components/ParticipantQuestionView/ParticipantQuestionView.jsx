76191611308c8be8c5229af4a4968b327ca2cd01
ryanmgray <gray.ryanm@gmail.com>
2017-02-25 10:21:20 -0800

    Add support for displaying all question types to participant

--- client/components/ParticipantQuestionView/ParticipantQuestionView.jsx
+++ client/components/ParticipantQuestionView/ParticipantQuestionView.jsx
@@ -6,6 +6,11 @@ class ParticipantQuestionView extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.showChoices = this.showChoices.bind(this);
+
+    this.state = {
+      value: 5
+    };
   }
 
   handleClick(e) {
@@ -15,13 +20,79 @@ class ParticipantQuestionView extends React.Component {
     });
   }
 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  showChoices() {
+    const questionType = this.props.questionType;
+    const handleClick = this.handleClick;
+    if (this.props.questionType === 'yn') {
+      return (
+        <div>
+          <button onClick={handleClick} value={1}>Yes</button>
+          <button onClick={handleClick} value={2}>No</button>
+        </div>
+      );
+    } else if (this.props.questionType === 'thumbs') {
+      return (
+        <div>
+          <button onClick={handleClick} value={1}>Thumbs Up</button>
+          <button onClick={handleClick} value={2}>Thumbs Middle</button>
+          <button onClick={handleClick} value={3}>Thumbs Down</button>
+        </div>
+      );
+    } else if (this.props.questionType === 'scale') {
+      console.log('hit scale');
+      return (
+        <div>
+          <pre>
+            1
+            <input
+              type="range"
+              min="0" max="10"
+              defaultValue={this.state.value}
+              onChange={this.handleChange.bind(this)}
+              step="1"/>
+            10
+            <button onClick={handleClick}>Submit</button>
+          </pre>
+        </div>
+      );
+    } else if (this.props.questionType === 'textarea') {
+      return (
+        <div>
+          <input type={questionType} onClick={handleClick} />
+          <button onClick={handleClick}>Submit</button>
+        </div>
+      );
+    } else {
+      return this.props.choices.map(function (choice) {
+        return (
+          <div>
+            <input type={questionType} name='choice' onClick={handleClick} /> { choice }
+          </div>
+        );
+      });
+    }
+  }
+
+  showSubmit() {
+    if (this.props.questionType === 'radio' || this.props.questionType === 'checkbox') {
+      return (
+        <div>
+          <button onClick={this.handleClick}>Submit</button>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className={styles.wrapper}>
         <h1>ParticipantQuestionView</h1>
-        <button onClick={this.handleClick} value={1}>Thumbs Up</button>
-        <button onClick={this.handleClick} value={2}>Thumbs Middle</button>
-        <button onClick={this.handleClick} value={3}>Thumbs Down</button>
+        { this.showChoices() }
+        { this.showSubmit() }
       </div>
     );
   }

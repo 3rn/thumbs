14c29479fc11daf14c2943c50b51b64899d705a7
ryanmgray <gray.ryanm@gmail.com>
2017-02-25 17:17:15 -0800

    Refactor component to render Visualization component and pass in appropriate graph data depending on questionType and choices

--- client/components/PresenterResultsView/PresenterResultsView.jsx
+++ client/components/PresenterResultsView/PresenterResultsView.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ThumbsCheckVis from '../ThumbsCheckVis/ThumbsCheckVis';
+import Visualization from '../Visualization/Visualization';
 import socket from '../../config/socket';
 
 class PresenterResultsView extends React.Component {
@@ -24,11 +24,35 @@ class PresenterResultsView extends React.Component {
   }
 
   render() {
+    if (this.props.questionType === 'thumbs') {
+      var graphData = [
+        {choice: 'Thumbs Up', value: this.props.data[0]},
+        {choice: 'Thumbs Middle', value: this.props.data[1]},
+        {choice: 'Thumbs Down', value: this.props.data[2]}
+      ];
+    } else if (this.props.questionType === 'yn') {
+      var graphData = [
+        {choice: 'Yes', value: this.props.data[0]},
+        {choice: 'No', value: this.props.data[1]}
+      ];
+    } else if (this.props.questionType === 'scale') {
+      var graphData = [
+        {choice: '1', value: this.props.data[0]},
+        {choice: '2', value: this.props.data[1]},
+        {choice: '3', value: this.props.data[2]},
+      ];
+    } else {
+      var graphData = [
+        {choice: 'A', value: this.props.data[0]},
+        {choice: 'B', value: this.props.data[1]},
+        {choice: 'C', value: this.props.data[2]}
+      ];
+    }
     return (
       <div>
         <h1>ResultsView</h1>
         <button onClick={this.handleClick}>{this.state.buttonName}</button>
-        <ThumbsCheckVis data={this.props.data} />
+        <Visualization data={graphData} questionType={this.props.questionType} choices={this.props.choices} />
       </div>
     );
   }

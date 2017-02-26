bab2f318e577c87565a2960ae0724afd9abda263
rwhuber <rwhuber13@gmail.com>
2017-02-25 16:43:12 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,11 @@ const app = express();
 
 const config = require('./server.config.js');
 const dbRoutes = require('./dbRoutes/dbRoutes.js');
-
-// const middleware = require('./middleware/middleware.js')(app, express);
 const helpers = require('./middleware/helpers.js')(app);
+
 app.use('/db', dbRoutes);
 
 const webpack = require('./middleware/webpack.js')(app, express);
-
 const server = app.listen(config.PORT, config.HOST, () => {
   console.log('Thumbs on at http://%s:%s', config.HOST, config.PORT);
 });

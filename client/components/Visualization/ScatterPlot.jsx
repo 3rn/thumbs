a2939873f4f3fbdec908cf0015f740d1449a2d8e
ryanmgray <gray.ryanm@gmail.com>
2017-02-25 17:28:23 -0800

    Add ScatterPlot component

---
+++ client/components/Visualization/ScatterPlot.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { VictoryChart, VictoryAxis, VictoryScatter, VictoryTheme } from 'victory';
+
+export default class ScatterPlot extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> scatter </h1>
+        <VictoryChart>
+          <VictoryAxis
+            />
+          <VictoryAxis
+            dependentAxis
+            />
+          <VictoryScatter
+          data={this.props.data}
+          x="choice"
+          y="value"
+        />
+    </VictoryChart>
+      </div>
+    );
+  }
+}

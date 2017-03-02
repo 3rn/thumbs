b251bfd711d8343003357bd9f43f790b88e7bccd
ryanmgray <gray.ryanm@gmail.com>
2017-03-01 19:47:53 -0800

    Fix response views to record votes correctly after refactoring intial state

--- client/components/Participant/ResponseViews/Thumbs.jsx
+++ client/components/Participant/ResponseViews/Thumbs.jsx
@@ -9,9 +9,9 @@ export default class Thumbs extends React.Component {
   render() {
     return (
       <div className={styles.card}>
-        <button className={styles.selectButton} onClick={this.props.click} value={1}>Thumbs Up</button>
-        <button className={styles.selectButton} onClick={this.props.click} value={2}>Thumbs Middle</button>
-        <button className={styles.selectButton} onClick={this.props.click} value={3}>Thumbs Down</button>
+        <button className={styles.selectButton} onClick={this.props.click} value={0}>Thumbs Up</button>
+        <button className={styles.selectButton} onClick={this.props.click} value={1}>Thumbs Middle</button>
+        <button className={styles.selectButton} onClick={this.props.click} value={2}>Thumbs Down</button>
       </div>
     );
   }

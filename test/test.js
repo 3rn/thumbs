cc0ac99da26f9dd3479b0eef7413542c88feff21
Richard Huber <rwhuber13@gmail.com>
2017-03-02 14:57:47 -0800

    Add mocha tests

---
+++ test/test.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+
+describe('Attendance', function() {
+  describe('Is Nathan here?', function() {
+    it('Nathan should be here', function() {
+      assert.equal(true, true);
+    });
+  });
+
+  describe('Is Richard here?', function() {
+    it('Richard should be here', function() {
+      assert.equal(true, true);
+    });
+  });
+
+  describe('Is Ryan here?', function() {
+    it('Ryan should be here', function() {
+      assert.equal(false, true);
+    });
+  });
+
+  describe('Is Ranit here?', function() {
+    it('Ryan should be here', function() {
+      assert.equal(false, true);
+    });
+  });
+});

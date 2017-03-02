0c6e8c2d3729085ceeff9c25e8fcb57ed1ee702e
ryanmgray <gray.ryanm@gmail.com>
2017-03-01 19:48:41 -0800

    Refactored reducers, adjust intial state object to handle multiple question types

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,13 +1,9 @@
 import { combineReducers } from 'redux';
 
-import thumbs from './voteReducer.js';
-import voteStatus from './voteStatusReducer.js';
-import participantCount from './participantCountReducer.js';
-import participantQuestion from './participantQuestionReducer.js';
+import participantReducer from './participantReducer.js';
+import presenterReducer from './presenterReducer.js';
 
 export default combineReducers({
-  thumbs,
-  voteStatus,
-  participantCount,
-  participantQuestion
+  participantReducer,
+  presenterReducer
 });
--- client/reducers/participantQuestionReducer.js
+++
@@ -1,8 +0,0 @@
-export default function participantQuestion(state = 0, action) {
-  switch (action.type) {
-    case 'PARTICIPANT_QUESTION':
-      const newCount = state + 1;
-      return newCount;
-  }
-  return state;
-}

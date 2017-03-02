04adc12f9f5b4bfd9bc3e39dc36d2bf808986d00
ryanmgray <gray.ryanm@gmail.com>
2017-03-01 19:46:04 -0800

    Fix MultipleChoice component to correctly submit vote

--- client/components/Participant/ResponseViews/MultipleChoice.jsx
+++ client/components/Participant/ResponseViews/MultipleChoice.jsx
@@ -4,16 +4,25 @@ import styles from '../../../styles/pages/_Response';
 export default class MultipleChoice extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      value: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.click = this.props.click.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
   }
 
   mapChoices() {
-    const questionType = this.props.questionType;
-    const click = this.props.click;
-
-    return this.props.choices.map(function (choice) {
+    const change = this.handleChange;
+    return this.props.choices.map(function (choice, i) {
       return (
         <div>
-          <input type={questionType} name='choice' onClick={click} /> { choice }
+          <input type="radio" name='choice' onClick={change} value={i} /> { choice }
         </div>
       );
     });
@@ -23,6 +32,7 @@ export default class MultipleChoice extends React.Component {
     return (
       <div className={styles.card}>
         { this.mapChoices() }
+        <button onClick={this.click} className={styles.selectButton} value={this.state.value}>Submit</button>
       </div>
     );
   }

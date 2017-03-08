15b0de88b86158fc18359e027e54cbf0581597f2
ntoung <nmtoung@gmail.com>
2017-03-08 11:43:08 -0800

    Update slide view and pull in lecture info

--- client/components/Presenter/SlideView.jsx
+++ client/components/Presenter/SlideView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
-
+import axios from 'axios';
+import { browserHistory } from 'react-router';
 import styles from '../../styles/pages/_SlideView';
 
 class SlideView extends React.Component {
@@ -8,22 +9,72 @@ class SlideView extends React.Component {
     super(props);
     
     this.state = {
-      'slideId': props.params.slideId
+      'slideId': props.params.slideId,
+      'slides': {},
+      'lecture': {}
     }
 
-    this.getSlides = this.getSlides.bind(this);
+    this.checkSlidesRoom = this.checkSlidesRoom.bind(this);
+    this.getLecture = this.getLecture.bind(this);
   }
 
-  getSlides() {
-    console.log('Getting slides');
-    //axios.get
+  componentWillMount() {
+    this.checkSlidesRoom();
+  }
+
+  checkSlidesRoom() {
+    const context = this;
+    return axios.get(`/db/s/${context.props.params.slideId}`)
+    .then(function (response) {
+      if (response.data.length === 0) {
+        browserHistory.push('/');
+      } else {
+        context.setState({slides: response.data[0]});
+        context.getLecture();
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  getLecture() {
+    const context = this;
+    axios.get(`/db/l/${this.state.slides.lecture_id}`)
+    .then(function (response) {
+      if (response) {
+        context.setState({
+          lecture: {
+            'title': response.data[0].title,
+            'created_at': (new Date(response.data[0].created_at)).toUTCString(),
+            'updated_at': (new Date(response.data[0].updated_at)).toUTCString(),
+            'description': response.data[0].description
+          }
+        });
+        console.log('LectureView: lecture ', context.state.lecture);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   render() {
     return (
       <div className={styles.wrapper}>
         <div className={styles.card}>
-          <h1>My SlideViews</h1>
+          <div className={styles.label}>Lecture Info</div>
+          <h1>{this.state.lecture.title}</h1>
+          <div className={styles.details}>
+            <strong>Last Updated: </strong>{this.state.lecture.updated_at}
+          </div>
+
+          <div className={styles.description}>
+            {this.state.lecture.description}
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.label}>Slides</div>
         </div>
       </div>
     );

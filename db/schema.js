54a849ca86238eeac106ac21db51e2f494afd2f2
ryanmgray <gray.ryanm@gmail.com>
2017-07-18 13:56:29 -0700

    Updated schema and fixed styling

--- db/schema.js
+++ db/schema.js
@@ -5,23 +5,23 @@
 const Sequelize = require('sequelize');
 
 if (process.env.PRODUCTION === 'production') {
-  var connection = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'thumbsdb.crmuzms0yo69.us-west-1.rds.amazonaws.com',
     dialect: 'postgres',
     port: 5432,
-    schema: 'public'
+    schema: 'public',
   });
 } else {
   // for localhost developement, use:
-  var connection = new Sequelize('thumbs', '', '', {
+  const connection = new Sequelize('thumbs', '', '', {
     dialect: 'postgres',
     port: 5432,
-    schema: 'public'
+    schema: 'public',
   });
 }
 
 // V0
-var SavedQuestions = connection.define('saved_questions',
+const SavedQuestions = connection.define('saved_questions',
   {
     'presentation_code': Sequelize.STRING,
     'title': Sequelize.STRING,
@@ -35,9 +35,7 @@ var SavedQuestions = connection.define('saved_questions',
 
 // Notes:
 // createdAt & updatedAt automatically included
-const define = function(model) {
-  return require(`./tables/${model}/${model}Model.js`)(connection, Sequelize);
-};
+const define = model => require(`./tables/${model}/${model}Model.js`)(connection, Sequelize);
 
 // V1
 

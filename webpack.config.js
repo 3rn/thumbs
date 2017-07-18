d59de27db0635db0b2b4fa18055d79ecb22f8fd7
ryanmgray <gray.ryanm@gmail.com>
2017-07-18 13:55:00 -0700

    Add .eslintrc file and updated package.json and webpack.config with new style guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,45 @@
-'use strict';
-
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
-
 module.exports = {
   devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'client/main.jsx')
+    path.join(__dirname, 'client/main.jsx'),
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
     filename: '[name].js',
-    publicPath: '/'
+    publicPath: '/',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'client/index.tpl.html',
       inject: 'body',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
-
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('development')
-      }
+        NODE_ENV: JSON.stringify('development'),
+      },
     }),
-
     // Optimization
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
-
     // gzip Compression
     new CompressionPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
       test: /\.js$|\.css$|\.html$/,
       threshold: 10240,
-      minRatio: 0.8
-    })
+      minRatio: 0.8,
+    }),
   ],
   module: {
     loaders: [
@@ -55,22 +49,22 @@ module.exports = {
           'style',
           'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
           // 'resolve-url',
-          'sass'
-        ]
+          'sass',
+        ],
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          'presets': ['react', 'es2015', 'stage-0', 'react-hmre']
-        }
+          presets: ['react', 'es2015', 'stage-0', 'react-hmre'],
+        },
       }, {
         test: /\.json?$/,
-        loader: 'json'
-      }]
+        loader: 'json',
+      }],
   },
   resolve: {
     extensions: ['', '.js', '.jsx', '.scss'],
-  }
+  },
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,59 +1,50 @@
-'use strict';
-
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const StatsPlugin = require('stats-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-
-const extractSass = new ExtractTextPlugin({
-  filename: '[name]-[hash].css',
-});
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry: [
-    path.join(__dirname, 'client/main.jsx')
+    path.join(__dirname, 'client/main.jsx'),
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
     filename: '[name]-[hash].min.js',
-    publicPath: '/'
+    publicPath: '/',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'client/index.tpl.html',
       inject: 'body',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
-    
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
-
-   // extractSass,
+    // extractSass,
     new ExtractTextPlugin('[name]-[hash].min.css'),
     new StatsPlugin('webpack.stats.json', {
       source: false,
-      modules: false
+      modules: false,
     }),
-
     // Optimization
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
-
     // gzip Compression
     new CompressionPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
       test: /\.js$|\.css$|\.html$/,
       threshold: 10240,
-      minRatio: 0.8
-    })
+      minRatio: 0.8,
+    }),
   ],
   module: {
     loaders: [{
@@ -61,24 +52,24 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel',
       query: {
-        'presets': ['es2015', 'stage-0', 'react']
-      }
+        presets: ['es2015', 'stage-0', 'react'],
+      },
     }, {
       test: /\.json?$/,
-      loader: 'json'
+      loader: 'json',
     }, {
       test: /\.css$/,
-      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]',
     }, {
       test: /\.scss$/,
       loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]!sass'),
-      exclude: /node_modules|lib/
-    }]
+      exclude: /node_modules|lib/,
+    }],
   },
   postcss: [
-    require('autoprefixer')
+    autoprefixer(),
   ],
   resolve: {
     extensions: ['', '.js', '.jsx', '.scss'],
-  }
+  },
 };

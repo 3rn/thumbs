40b2d07021d0203170fb2713d2000abb829b9e2e
ryanmgray <gray.ryanm@gmail.com>
2017-07-18 15:17:49 -0700

    Update redux reducers and client side config files

--- client/config/socket.js
+++ client/config/socket.js
@@ -1,10 +1,10 @@
 import io from 'socket.io-client';
 
 if (process.env.NODE_ENV === 'production') {
-  var socket = io('http://thumbsup.us-west-1.elasticbeanstalk.com/');
+  const socket = io('http://thumbsup.us-west-1.elasticbeanstalk.com/');
 } else {
   // for localhost developement, use:
-  var socket = io('http://localhost:8000/');
+  const socket = io('http://localhost:8000/');
 }
 
 export default socket;
